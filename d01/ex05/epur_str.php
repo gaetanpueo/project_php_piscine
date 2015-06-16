40ef2719dd11f23ee346b8afb6c2dd34528417db
Gaetan PUEO--GOURNAY <gpueo--g@e1r12p17.42.fr>
2015-06-16 16:08:33 +0200

    d01 | ex00(f) - ex01(f) - ex02(f) - ex03(f) - ex04(f) - ex05(f) - ex06(e)

--- d01/ex05/epur_str.php
+++ d01/ex05/epur_str.php
@@ -1,1 +1,18 @@
 #!/usr/bin/php
+<?php
+if ($argc === 2)
+{
+	$start = 0;
+	for ($i = 0; $i < strlen($argv[1]); $i++)
+	{
+		if ($argv[1][$i] != " ")
+		{
+			if ($argv[1][$i - 1] === " " && $start === 1)
+				echo " ";
+			$start = 1;
+			echo $argv[1][$i];
+		}
+	}
+	echo "\n";
+}
+?>
