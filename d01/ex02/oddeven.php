fdcd5637e71d59f3c89821e9897e5be2f6ec5d3d
Gaetan PUEO--GOURNAY <gpueo--g@e1r12p17.42.fr>
2015-06-16 13:37:02 +0200

    d00 | ex00(f) - ex01(f) - ex02(f)

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -1,4 +1,23 @@
 #!/usr/bin/php
 <?php
-
+function get_nbr()
+{
+	echo "Entrez un nombre: ";
+	$f = fopen('php://stdin', 'r');
+	if (($line = fgets($f)) == NULL)
+	{
+		echo "\n";
+		exit();
+	}
+	$var = trim($line, "\n");
+	if (ctype_digit($var) == FALSE)
+		echo "'$var' n'est pas un chiffre\n";
+	elseif ($var % 2 == 0)
+		echo "Le chiffre $var est Pair\n";
+	elseif ($var % 2 != 0)
+		echo "Le chiffre $var est Impair\n";
+	fclose($f);
+}
+while (42)
+	get_nbr();
 ?>
