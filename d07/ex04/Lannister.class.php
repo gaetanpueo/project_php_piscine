cf29e3ec83676b1b64c3c94d0150e3c6be0ee625
Gaetan Pueo <gaetanpueo@Gaetans-MacBook-Pro.local>
2015-06-25 16:33:22 +0200

    d07

--- d07/ex04/Lannister.class.php
+++ d07/ex04/Lannister.class.php
@@ -2,7 +2,16 @@
 
 Class Lannister
 {
-	
+	protected $_family = True;
+	public function sleepWith($partner)
+	{
+		if (!isset($partner->_family))
+			print("Let's do this.\n");
+		else if ($partner->_family == $this->_family)
+			print("With pleasure, but only in a tower in Winterfell, then.\n");
+		else
+			print("Not even if I'm drunk !\n");
+	}
 }
 
 ?>
