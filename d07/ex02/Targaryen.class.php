e50b4b5ba9b2a9fdae4562e5613e395f18e73371
Gaetan Pueo <gaetanpueo@Gaetans-MacBook-Pro.local>
2015-06-25 16:31:05 +0200

    d07

--- d07/ex02/Targaryen.class.php
+++ d07/ex02/Targaryen.class.php
@@ -2,7 +2,16 @@
 
 Class Targaryen
 {
-	
+	public function resistsFire()
+	{
+		return (False);
+	}
+	public function getBurned()
+	{
+		if (static::resistsFire())
+			return ("emerges naked but unharmed");
+		return ("burns alive");
+	}
 }
 
 ?>
--- d07/ex03/House.class.php
+++ d07/ex03/House.class.php
@@ -1,6 +1,8 @@
 <?php
-class House
+
+Class House
 {
 	
 }
+
 ?>
