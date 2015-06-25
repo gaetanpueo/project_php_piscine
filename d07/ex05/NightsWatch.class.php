2dd28d75502debe35f9ec85b596a6c26b387b63f
Gaetan Pueo <gaetanpueo@Gaetans-MacBook-Pro.local>
2015-06-25 10:48:46 +0200

    d07

---
+++ d07/ex05/NightsWatch.class.php
@@ -0,0 +1,16 @@
+<?php
+class NightsWatch implements IFighter
+{
+	public function fight()
+	{
+	}
+	public function recruit($att)
+	{
+		$class = get_class($att);
+		if ($class == "JonSnow")
+			JonSnow::fight();
+		if ($class == "SamwellTarly")
+			SamwellTarly::fight();
+	}
+}
+?>
